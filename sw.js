acfd9e7e6ddab3667974ac6ccf6307d4e62063c6
q2418100271 <q2418100271@qq.com>
2021-02-09 19:21:41 +0800

    Site updated: 2021-02-09 19:21:40

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('active', function () {
 });
 
 var precacheUrls = [
-'/','/posts/43789/','/posts/36215/','/posts/22164/','/about/index.html','/artitalk/index.html','/extension/content.json','/icons/index.html','/gallery/index.html','/letter/index.html','/library/index.html','/resume/index.html','/records/content.json','/records/index.html','/extension/letter/index.js','/extension/letter/index.css','/extension/test/index.html','/extension/resume/index.js','/extension/resume/index.css','/extension/letter/chinese/data.json','/extension/letter/chinese/index.html','/extension/letter/english/data.json','/extension/letter/english/index.html','/extension/resume/chinese/data.json','/extension/resume/chinese/index.html','/extension/resume/english/data.json','/extension/resume/english/index.html',
+'/','/posts/43789/','/posts/36215/','/posts/22164/','/about/index.html','/gallery/index.html','/artitalk/index.html','/icons/index.html','/extension/content.json','/letter/index.html','/library/index.html','/records/content.json','/resume/index.html','/records/index.html','/extension/test/index.html','/extension/letter/index.js','/extension/letter/index.css','/extension/resume/index.css','/extension/resume/index.js','/extension/letter/chinese/index.html','/extension/letter/chinese/data.json','/extension/letter/english/index.html','/extension/letter/english/data.json','/extension/resume/chinese/data.json','/extension/resume/chinese/index.html','/extension/resume/english/data.json','/extension/resume/english/index.html',
 ];
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
